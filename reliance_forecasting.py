06b955ea27df0fbf08985aabb7a9efa2378961e5
Manish Kumar Rai <manishkumarrai3389@gmail.com>
2024-08-07 18:29:36 +0530

    changing arima order

--- reliance_forecasting.py
+++ reliance_forecasting.py
@@ -31,7 +31,7 @@ def plot_data(data, ax):
 
 # Function to forecast data
 def forecast(data):
-    model = ARIMA(data['Close'], order=(5, 1, 0))
+    model = ARIMA(data['Close'], order=(5, 1, 0.1))
     model_fit = model.fit()
     forecast_data = model_fit.forecast(steps=30)
     return forecast_data
