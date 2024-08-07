fc2c5fdb1777d6468791cffe9b40b90c7df89459
Manish Kumar Rai <manishkumarrai3389@gmail.com>
2024-08-07 18:30:53 +0530

    playing with forecast

--- reliance_forecasting.py
+++ reliance_forecasting.py
@@ -31,9 +31,9 @@ def plot_data(data, ax):
 
 # Function to forecast data
 def forecast(data):
-    model = ARIMA(data['Close'], order=(5, 1, 0.1))
+    model = ARIMA(data['Close'], order=(5, 1, 0))
     model_fit = model.fit()
-    forecast_data = model_fit.forecast(steps=30)
+    forecast_data = model_fit.forecast(steps=50)
     return forecast_data
 
 # Function to plot forecast
